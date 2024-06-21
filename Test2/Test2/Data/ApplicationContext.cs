b122920692b3795cb07e457aeae6d7ba8b8506bc
MykhailoOl <mishaoleynik8@gmail.com>
2024-06-21 15:03:44 +0200

    Pattern

---
+++ Test2/Test2/Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Test2.Data;
+
+public class ApplicationContext : DbContext
+{
+    protected ApplicationContext()
+    {
+    }
+
+    public ApplicationContext(DbContextOptions options) : base(options)
+    {
+    }
+    //public DbSet<Class> name{get;set;}
+}
