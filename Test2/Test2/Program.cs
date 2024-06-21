d52dfc2701689ca5fe70c27a47478baf7893bd38
MykhailoOl <mishaoleynik8@gmail.com>
2024-06-21 15:07:43 +0200

    Pattern Update

--- Test2/Test2/Program.cs
+++ Test2/Test2/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Test2.Data;
+using Test2.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddScoped<IDbService, DbService>();
 
 var app = builder.Build();
 
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
 
---
+++ Test2/Test2/Services/DbService.cs
@@ -0,0 +1,12 @@
+﻿using Test2.Data;
+
+namespace Test2.Services;
+
+public class DbService : IDbService
+{
+    private readonly ApplicationContext _context;
+    public DbService(ApplicationContext context)
+    {
+        _context = context;
+    }
+}
